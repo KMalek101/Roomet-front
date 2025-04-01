c18330cc6e367c0a907a3c2b30e133f800649fff
Malek <kaoucheaneslive@gmail.com>
2025-04-01 17:52:42 +0100

    Padding fix

--- src/components/common/singup/Signup.jsx
+++ src/components/common/singup/Signup.jsx
@@ -1,6 +1,6 @@
 export function SignUp() {
     return(
-        <div className="w-min flex flex-col items-center justify-center p-52 border-2 border-[var(--g-color)] ">
+        <div className="w-min flex flex-col items-center justify-center p-12 border-2 border-[var(--g-color)] ">
             <h2>Sign Up</h2>
             <form action="" className="p-6">
                 <input style={{width : "380px"}} className='px-4 py-2 text-xl text-[var(--black-color)] placeholder-[var(--g-color)] rounded-md border border-[var(--main-color-hover' type="text"
