11351cb069bde50e37e58b474d52bc5193a331e1
Malek <kaoucheaneslive@gmail.com>
2025-04-09 12:46:20 +0100

    createBlocks ui update

--- src/app/blocks/page.jsx
+++ src/app/blocks/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import CreateBlocks from "@/components/common/create blocks/CreateBlocks";
 import Header from "@/components/common/header/Header";
 import Sidebar from "@/components/common/side bar/Sidebar";
 
@@ -9,8 +10,10 @@ export default function page() {
       <Header />
       <div className="flex">
         <Sidebar />
-        <p className="font-semibold text-2xl">Create your blocks</p>
-        
+        <div className="flex flex-col py-6 px-12 flex-1 ">
+          <p className="font-semibold text-2xl pb-4">Create your blocks</p>
+          <CreateBlocks />
+        </div>
       </div>
     </div>
   );
