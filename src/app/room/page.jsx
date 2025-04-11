61fe726a3c563843ca51585d7434073db3c5455a
Malek <kaoucheaneslive@gmail.com>
2025-04-11 15:50:25 +0100

    add button

--- src/app/room/page.jsx
+++ src/app/room/page.jsx
@@ -6,7 +6,7 @@ import Sidebar from "@/components/common/side bar/Sidebar";
 
 export default function page() {
   return (
-    <div className="h-screen w-screen overflow-hidden">
+    <div className="h-screen w-screen overflow-hidden relative">
       <Header />
       <div className="flex overflow-hidden">
         <Sidebar />
@@ -15,6 +15,11 @@ export default function page() {
           <ViewRooms />
         </div>
       </div>
+      
+      <button className="cursor-pointer flex items-center justify-center absolute h-12 w-12 bottom-12 right-18 bg-[var(--green-color)] rounded-md opacity-50 hover:opacity-100 transition-all duration-25 ease-in">
+        <div className="flex h-1/3 w-[1px] bg-[var(--w-color)] absolute"></div>
+        <div className="w-1/3 h-[1px] bg-[var(--w-color)] absolute"></div>
+      </button>
     </div>
   );
 }
