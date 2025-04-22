d8955630c667392ac13bc567b3afc89c0f773563
Malek <kaoucheaneslive@gmail.com>
2025-04-22 18:14:41 +0100

    Reports page initializing

--- src/app/reports/page.jsx
+++ src/app/reports/page.jsx
@@ -1,4 +1,20 @@
+'use client'
+
+import Header from "@/components/common/header/Header";
+import Sidebar from "@/components/common/side bar/Sidebar";
+import Reports from "@/components/reports/Reports";
+
 export default function page() {
-  return <div className="bg-[#292929] w-screen h-screen">
-  </div>;
+  return (
+    <div className="h-screen w-screen overflow-hidden relative">
+      <Header />
+      <div className="flex overflow-hidden">
+        <Sidebar />
+        <div className="flex flex-col py-6 px-12 flex-1 overflow-y-auto h-screen no-scrollbar pb-24">
+          <p className="font-semibold text-2xl pb-4">Reports</p>
+          <Reports />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/reports/Reports.jsx
@@ -0,0 +1,7 @@
+export default function Reports() {
+    return(
+        <div className="flex flex-col gap-4 bg-[var(--secondary-color)] p-6 rounded-md flex-1 relative">
+            
+        </div>
+    )
+}
