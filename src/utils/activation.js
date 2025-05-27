112bfe97a6cc9982e4498ef92b0656939c5b35dc
Malek <kaoucheaneslive@gmail.com>
2025-05-27 14:42:59 +0100

    activation working for both

--- src/utils/activation.js
+++ src/utils/activation.js
@@ -3,7 +3,7 @@ const API_BASE_URL = "http://localhost:5000";
 
 export async function activateUser(token, data) {
   try {
-    const response = await axios.post(`${API_BASE_URL}/api/activate/${token}`, data, {
+    const response = await axios.post(`${API_BASE_URL}/api/auth/activate/${token}`, data, {
       withCredentials: true,
       headers: {
         'Content-Type': 'application/json'
